5505cc7c59e49a0799fa0aafca9b87b30b6817dc
Anatoliy <boldinao@icloud.com>
2018-12-12 18:55:22 +0500

    profile gluing

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,23 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-
+            string name, firstName, age, growth, weight;
+            Console.Write("Имя: ");
+            name = Console.ReadLine();
+            Console.Write("Фамилия: ");
+            firstName = Console.ReadLine();
+            Console.Write("Возраст: ");
+            age = Console.ReadLine();
+            Console.Write("Рост: ");
+            growth = Console.ReadLine();
+            Console.Write("Вес: ");
+            weight = Console.ReadLine();
+            Console.Write("Имя: " + name +
+                "\n Фамилия: " + firstName +
+                "\n Возраст: " + age +
+                "\n Рост: " + growth +
+                "\n Вес: " + weight);
+            Console.ReadKey();
         }
     }
 }
